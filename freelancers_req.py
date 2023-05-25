eee820a45eb7dd21184a71ba101870b7dcd35281
Michal <miso.zima@1337tester.com>
2023-05-25 21:10:20 +0200

    enhance request

--- freelancers_req.py
+++ freelancers_req.py
@@ -6,6 +6,7 @@ import os
 import datetime
 from random import randint
 import click
+import logging
 
 # relevant data for searches
 keyword = "test"
@@ -16,20 +17,51 @@ csv_file = os.path.join(dir_path, "jobs_" + keyword + ".csv")
 timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 csv_file_timestamped = os.path.join(dir_path, "jobs_" + keyword + timestamp + ".csv")
 random_search_id = randint(100000000000000000000000000000000, 999999999999999999999999999999999)
-data_test_zurich = {'__seo_search':'search',
+random_search_id = '92d0f0f5acecfae00c446527944e861a'
+data_test_zurich = {
+                    '__search_sort_by':'1',
+                    '__search_project_age':'0',
+                    '__search_profile_availability':'0',
+                    '__search_profile_update':'0',
+                    '__search_profile_apply_watchlist':'0',
+                    '__search_project_start_date':'0',
+                    '__search_profile_ac':'0',
+                    '__search_additional_filter':'0',
+                    '__seo_search':'0',
+                    '__seo_search_extended':'0',
                     '__search_freetext':keyword,
                     '__search_city':location[0],
-                    'seal':random_search_id,
+                    'seal':'ac02aa76af9cc765a4c95d86a52cfa27f4708d63',
                     '__search_city_location_id':location[1],
                     '__search_city_country':location[2],
+                    '__search_city_country_extended':'0',
                     '__search_city_perimeter':'100',
                     'search_id':random_search_id,
-                    'search_simple':'suchen'}
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
+                    'search_simple':'suchen',
+                    '__search_country':'0',
+                    '__search_hour_rate_modifier':'0',
+                    '__search_experience_modifier':'0',
+                    '__search_additional_filter':'0',
+                    '__search_project_age_remote':'0',
+                    '__search_project_start_date_remote':'0'
+                    }
+headers = {
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Language':'en-US,en;q=0.5',
+        'Accept-Encoding':'gzip, deflate, br',
+        'Content':'application/x-www-form-urlencoded',
+        'Connection':'keep-alive',
+        'Host':'www.freelance.de',
         'Origin':'https://www.freelance.de',
         'Referer':'https://www.freelance.de/Projekte/K/IT-Entwicklung-Projekte/?_offset=',
-        'Connection':'keep-alive'}
+        'Sec-Fetch-Dest':'document',
+        'Sec-Fetch-Mode':'navigate',
+        'Sec-Fetch-Site':'same-origin',
+        'Sec-Fetch-User':'?1',
+        'TE':'trailers',
+        'Upgrade-Insecure-Requests':'1',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0'
+        }
 
 def process_jobs(csv_file, all_jobs, csv_file_timestamped) -> None:
     if os.path.exists(csv_file):
@@ -87,6 +119,8 @@ def parse_website(website):
         pagination1 = soup1.find('div', id='pagination').p
         next_pages = math.ceil((int(pagination1.text.split()[3]) - 20) / 20)
         
+        logging.debug(f"Number of pages - {next_pages}")        
+        
         # go through the next pages and scrape
         for pages in range(1, next_pages + 1):
             offset = pages * 20
@@ -96,9 +130,9 @@ def parse_website(website):
             all_jobs = jobs_info(soup_next, all_jobs, website)
             
             # following is to doublecheck if we are still in the same search results
-            # pagination_next = soup_next.find('div', id='pagination').p
-            # print(url_next)   
-            # print(pagination_next.text.split())
+            pagination_next = soup_next.find('div', id='pagination').p
+            print(url_next)   
+            print(pagination_next.text.split())
         
         # read csv file and write into it new entries
         process_jobs(csv_file, all_jobs, csv_file_timestamped)
